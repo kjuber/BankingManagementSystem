6a96bf31b1083df2b484cdc42d5cdba246bdf515
kjuber <100750673+kjuber@users.noreply.github.com>
2022-03-02 11:46:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,47 +17,15 @@ class Account:
             input("Enter The Initial amount(>=500 for Saving and >=1000 for current"))
         print("\n\n\nAccount Created")
 
-    def showAccount(self):
-        print("Account Number : ", self.accNo)
-        print("Account Holder Name : ", self.name)
-        print("Type of Account", self.type)
-        print("Balance : ", self.deposit)
-
-    def modifyAccount(self):
-        print("Account Number : ", self.accNo)
-        self.name = input("Modify Account Holder Name :")
-        self.type = input("Modify type of Account :")
-        self.deposit = int(input("Modify Balance :"))
-
-    def depositAmount(self, amount):
-        self.deposit += amount
-
-    def withdrawAmount(self, amount):
-        self.deposit -= amount
-
-    def report(self):
-        print(self.accNo, " ", self.name, " ", self.type, " ", self.deposit)
-
-    def getAccountNo(self):
-        return self.accNo
-
-    def getAcccountHolderName(self):
-        return self.name
-
-    def getAccountType(self):
-        return self.type
-
-    def getDeposit(self):
-        return self.deposit
 
 
 def intro():
     print("\t\t\t\t**********************")
-    print("\t\t\t\tFAYAZ BANK OF NXTWAVE")
+    print("\t\t\t\tJUBER BANK OF INDIA")
     print("\t\t\t\t**********************")
 
     print("\t\t\t\tBrought To You By:")
-    print("\t\t\t\tFayaz CCBPian")
+    print("\t\t\t\tJUBER BASHA")
     input()
 
 
@@ -218,7 +186,7 @@ while ch != 8:
         num = int(input("\tEnter The account No. : "))
         modifyAccount(num)
     elif ch == '8':
-        print("\tThanks for using Fayaz bank Of Nxtwave")
+        print("\tThanks for using JUBER bank Of INDIA")
         break
     else:
         print("Invalid choice")
